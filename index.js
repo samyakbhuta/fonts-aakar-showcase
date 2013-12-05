66cd1e90f702a2592b4f6e026b4851576105c28e
Samyak Bhuta <samyak.bhuta@gmail.com>
2013-12-06 05:02:47 +0530

    Allows fontsize to be specified. Fixes #5.
    - Font size is optional.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var exphbs  = require('express3-handlebars');
 var app = express();
 app.use(express.logger());
 
-// [todo] : Support for headerlevel/fontsize in URL
 // [todo] : Support for color in URL
 // [todo] : Support for background-color in URL
 // [todo] : '/' URL should display usage instruction and other info.
@@ -19,8 +18,10 @@ app.set('view engine', 'handlebars');
 // But putting such artificial filter doesn't look 'hackable'.
 // Avoiding Gujarati Unicode Only check.
 
-app.get('/:text', function(req, res){
-  res.render('showcase-page',{text:req.params.text});
+app.get('/:text/:fontSize?', function(req, res){
+  res.render('showcase-page',{ text:req.params.text,
+ 	fontSize : req.params.fontSize+"px"
+  });
 });
 
 app.listen(3000);
