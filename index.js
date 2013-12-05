34a027634f652a242bd2a1115537ec0954ba3c7e
Samyak Bhuta <samyak.bhuta@gmail.com>
2013-12-06 04:08:38 +0530

    Echoes the path text.
    - Sends it as H1 level header.
    - We are using handlebar templating

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var express = require('express');
+var exphbs  = require('express3-handlebars');
 var app = express();
+app.use(express.logger());
 
-app.get('/', function(req, res){
-  res.send('આકાર');
+// [todo] : Support for fontsize in URL
+// [todo] : Support for color in URL
+// [todo] : Support for background-color in URL
+
+app.set('title', 'Aakar Font Showcase');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+
+// Ideally this app is meant to showcase Gujarati Unicode text.
+// :text parameter should only respond to Gujarati Unicode text
+// But putting such artificial filter doesn't look 'hackable'.
+// Avoiding Gujarati Unicode Only check.
+
+app.get('/:text', function(req, res){
+  res.render('showcase-page',{text:req.params.text});
 });
 
 app.listen(3000);
